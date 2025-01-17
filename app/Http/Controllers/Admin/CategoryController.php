cd60534624103b02088437f8330cbd6ee80713aa
Payamifard <a.payamifard@yahoo.com>
2025-01-17 23:48:19 +0330

    admin controllers namespace change,refactor routes

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 use Illuminate\Support\Str;
 
 use App\Models\Category;
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\admin\ColorController;
+use App\Http\Controllers\Admin\ColorController;
 use App\Http\Controllers\Admin\SliderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Frontend\FrontendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,71 +19,61 @@ use App\Http\Controllers\Admin\CategoryController;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index'] )->name('frontend');
+// Frontend Routes
+Route::get('/', [FrontendController::class, 'index'])->name('frontend');
+Route::get('/categories/{category_slug}', [FrontendController::class, 'products'])->name('frontend.products');
 
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/categories/{category_slug}',[App\Http\Controllers\Frontend\FrontendController::class, 'products'] )->name('frontend.products');
+// Admin Routes
+Route::prefix('/admin')->middleware(['auth', 'isAdmin'])->group(function () {
 
+    // Dashboard Route
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-//adminmiddleware ra dar kernel ba esme isadmin tarif kardim va inja esefade mikonim
-//un auth dar parantez ham dare middlware authentication ro seda mikone yani baraye dastresi be in route niaz be login has
-route::prefix('/admin')->middleware(['auth','isAdmin'])->group(function(){
-
-    route::get('/dashboard', [App\Http\Controllers\admin\DashboardController::class, 'index'])->name('dashboard') ;
-
+    // Category Routes
     Route::controller(CategoryController::class)->group(function () {
-        route::get('/category', 'index')->name('category.index') ;
-        route::get('/category/create', 'create')->name('category.create') ;
-        route::post('/category', 'store')->name('category.store') ;
-        route::get('/category/edit/{category}', 'edit')->name('category.edit') ;
-        route::put('/category/{category}', 'update')->name('category.update') ;
+        Route::get('/category', 'index')->name('category.index');
+        Route::get('/category/create', 'create')->name('category.create');
+        Route::post('/category', 'store')->name('category.store');
+        Route::get('/category/edit/{category}', 'edit')->name('category.edit');
+        Route::put('/category/{category}', 'update')->name('category.update');
     });
 
-    route::get('/brands',App\Livewire\Admin\Brand\Index::class)->name('brand.index') ;
+    // Brand Routes
+    Route::get('/brands', App\Livewire\Admin\Brand\Index::class)->name('brand.index');
 
-    //route::get('/products/edit/{productid}',App\Livewire\Admin\Products\Edit::class)->name('product.edit') ;
-
-
+    // Product Routes
     Route::controller(ProductController::class)->group(function () {
-        route::get('/products', 'index')->name('product.index') ;
-        route::get('/products/create', 'create')->name('product.create') ;
-        route::post('/products', 'store')->name('product.store') ;
-        //
-        route::get('/products/edit/{productid}', 'edit')->name('product.edit') ;
-        route::put('/products/{product}', 'update')->name('product.update') ;
-        route::get('/products', 'index')->name('product.index') ;
-        route::get('/product-image/delete/{product_image_id}', 'destroyImage')->name('product.deleteImage') ;
-        // route::get('/product/delete/{product_id}', 'destroyProduct')->name('product.destroy') ;
-        route::post('/product-color/{prod_color_id}','updateProdColorQty') ;
-        route::get('/product-color/{prod_color_id}/delete','deleteProdColor') ;
+        Route::get('/products', 'index')->name('product.index');
+        Route::get('/products/create', 'create')->name('product.create');
+        Route::post('/products', 'store')->name('product.store');
+        Route::get('/products/edit/{product}', 'edit')->name('product.edit');
+        Route::put('/products/{product}', 'update')->name('product.update');
+        Route::get('/product-image/delete/{product_image_id}', 'destroyImage')->name('product.deleteImage');
+        Route::post('/product-color/{prod_color_id}', 'updateProdColorQty');
+        Route::get('/product-color/{prod_color_id}/delete', 'deleteProdColor');
     });
 
+    // Color Routes
     Route::controller(ColorController::class)->group(function () {
-        route::get('/colors', 'index')->name('colors.index') ;
-        route::get('/colors/create', 'create')->name('colors.create') ;
-        route::post('/colors/create', 'store')->name('colors.store') ;
-        route::get('/colors/edit/{color}', 'edit')->name('colors.edit') ;
-        route::put('/colors/edit/{color}', 'update')->name('colors.update') ;
-        route::get('/colors/delete/{color}', 'destroy')->name('colors.destroy') ;
+        Route::get('/colors', 'index')->name('colors.index');
+        Route::get('/colors/create', 'create')->name('colors.create');
+        Route::post('/colors/create', 'store')->name('colors.store');
+        Route::get('/colors/edit/{color}', 'edit')->name('colors.edit');
+        Route::put('/colors/edit/{color}', 'update')->name('colors.update');
+        Route::get('/colors/delete/{color}', 'destroy')->name('colors.destroy');
     });
 
-
+    // Slider Routes
     Route::controller(SliderController::class)->group(function () {
-
-        route::get('/sliders','index')->name('sliders.index') ;
-        route::get('/sliders/create','create')->name('sliders.create') ;
-        route::post('/sliders/create','store')->name('sliders.store') ;
-        route::get('/sliders/edit/{slider}','edit')->name('sliders.edit') ;
-        route::put('/sliders/edit/{slider}','update')->name('sliders.update') ;
-        route::get('/sliders/delete/{slider}','destroy')->name('sliders.destroy') ;
+        Route::get('/sliders', 'index')->name('sliders.index');
+        Route::get('/sliders/create', 'create')->name('sliders.create');
+        Route::post('/sliders/create', 'store')->name('sliders.store');
+        Route::get('/sliders/edit/{slider}', 'edit')->name('sliders.edit');
+        Route::put('/sliders/edit/{slider}', 'update')->name('sliders.update');
+        Route::get('/sliders/delete/{slider}', 'destroy')->name('sliders.destroy');
     });
 
-
-
-}) ;
-
-
-
+});
